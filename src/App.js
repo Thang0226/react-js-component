fbbac8d431f4c5b7c29bf73d1d3db250d5322eeb
Thang0226 <thang.nd0226@gmail.com>
2025-01-21 16:28:48 +0700

    add bootstrap

--- src/App.js
+++ src/App.js
@@ -6,8 +6,10 @@ import {Component} from "react";
 
 // function App() {
 //   return (
-//     // <Alert text="Cảnh báo! Tài nguyên bạn vừa truy cập không tồn tại."/>
-//       <Student/>
+//       <>
+//           <Alert text="Cảnh báo! Tài nguyên bạn vừa truy cập không tồn tại."/>
+//           <Student/>
+//       </>
 //   );
 // }
 
